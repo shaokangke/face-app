fc46e062247a91a2ed42fd775b0e51ebeff8d4ec
shaokangke <fame_keshaokang@163.com>
2026-03-03 11:15:16 +0800

    Update faceDetection.jsx

--- src/pages/faceDetection/faceDetection.jsx
+++ src/pages/faceDetection/faceDetection.jsx
@@ -73,8 +73,7 @@ const FaceDetectionComponent = () => {
       <h1>人脸标记Demo</h1>
       <img ref={imageRef} src={pic} alt="Your Image" />
       <div>哈哈</div>
-      <div>快看看</div>
-      <div>发发发</div>
+      <div>111</div>
     </div>
   );
 };
